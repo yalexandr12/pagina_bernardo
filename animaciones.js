584b46aa87538151a21f859bb62ba48972a5acbb
Yael <yalexandr12@gmail.com>
2023-11-27 23:10:18 -0600

    Nuevos cambios

--- animaciones.js
+++ animaciones.js
@@ -1,27 +1,2 @@
-// script.js
 
-$(document).ready(function () {
-    $(window).scroll(function () {
-      var windowHeight = $(window).height();
-      var scrollPosition = $(window).scrollTop();
-      var elementPosition = $("#animated-button").offset().top;
   
-      if (scrollPosition + windowHeight > elementPosition) {
-        $("#animated-button").addClass("animate__animated animate__fadeInRight");
-        $("#animated-button").css("opacity", "1");
-      }
-    });
-  });
-  
-  
-  $(document).ready(function () {
-    $(window).scroll(function () {
-      var windowHeight = $(window).height();
-      var scrollPosition = $(window).scrollTop();
-      var elementPosition = $(".blue-box").offset().top;
-
-      if (scrollPosition + windowHeight > elementPosition) {
-        $(".blue-box").addClass("animate__animated animate__fadeIn");
-      }
-    });
-  });
